5b96a00afcf1a013f968190e0340d4bea10401cc
soulchild <soulchildsince2013@gmail.com>
2018-06-20 19:21:21 +0800

    Refactored test and added comments
    Refactored test and added comments
    
    Signed-off-by: soulchild <soulchildsince2013@gmail.com>

--- loginTestAppTests/LoginViewControllerTests.swift
+++ loginTestAppTests/LoginViewControllerTests.swift
@@ -8,15 +8,29 @@
 
 import XCTest
 
-// IMPORTANT: import the 'loginTestApp' (your app) target and its source file so we can use the classes in test
+// import the 'loginTestApp' (your app) target and its source file so we can use the classes in test
 @testable import loginTestApp
 
 class LoginViewControllerTests: XCTestCase {
-    
+	
+	// this method will be called before every test function is called
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+		
+		let storyboard = UIStoryboard(name: "Main", bundle: nil)
+		let loginVC = storyboard.instantiateInitialViewController() as! LoginViewController
+		
+		// call this so that the view controller's view will be instantiated
+		// else its view wont load and we cant access the labels / buttons on it
+		let _ = loginVC.view
+		
+		// Set Login View Controller as the current view on the (imaginary) iOS device
+		let window = UIWindow(frame: UIScreen.main.bounds)
+		window.makeKeyAndVisible()
+		window.rootViewController = loginVC
+		_ = loginVC.view
+	}
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
@@ -24,24 +38,14 @@ class LoginViewControllerTests: XCTestCase {
     }
     
     func testErrorMessageShouldBeHiddenAtFirst() {
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		let loginVC = storyboard.instantiateInitialViewController() as! LoginViewController
-		
-		// call this so that the view controller's view will be instantiated
-		// else its view wont load and we cant access the labels / buttons on it
-		let _ = loginVC.view
+		let loginVC = UIApplication.shared.keyWindow?.rootViewController as! LoginViewController
 		
 		// Error message should be hidden when the view controller is first loaded
 		XCTAssertTrue(loginVC.errorMessageLabel.isHidden)
     }
 	
 	func testUsernameErrorShouldShowWhenUsernameIsBlank(){
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		let loginVC = storyboard.instantiateInitialViewController() as! LoginViewController
-		
-		// call this so that the view controller's view will be instantiated
-		// else its view wont load and we cant access the labels / buttons on it
-		let _ = loginVC.view
+		let loginVC = UIApplication.shared.keyWindow?.rootViewController as! LoginViewController
 		
 		// simulate login button tap by calling the IBAction method
 		loginVC.loginTapped(loginVC.loginButton)
@@ -52,12 +56,7 @@ class LoginViewControllerTests: XCTestCase {
 	}
 	
 	func testPasswordErrorShouldShowWhenPasswordIsBlankAndUsernameIsNotBlank(){
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		let loginVC = storyboard.instantiateInitialViewController() as! LoginViewController
-		
-		// call this so that the view controller's view will be instantiated
-		// else its view wont load and we cant access the labels / buttons on it
-		let _ = loginVC.view
+		let loginVC = UIApplication.shared.keyWindow?.rootViewController as! LoginViewController
 		
 		// simulate fill in username text field
 		loginVC.usernameTextField.text = "axelkee"
@@ -70,4 +69,20 @@ class LoginViewControllerTests: XCTestCase {
 		XCTAssertEqual(loginVC.errorMessageLabel.text, "Password cannot be empty")
 	}
 	
+	func testAlertShouldAppearWhenLoginIsSuccessful(){
+		let loginVC = UIApplication.shared.keyWindow?.rootViewController as! LoginViewController
+		
+		// simulate fill in username and password
+		loginVC.usernameTextField.text = "axelkee"
+		loginVC.passwordTextField.text = "supersecretpassword"
+		
+		// simulate login button tap by calling the IBAction method
+		loginVC.loginTapped(loginVC.loginButton)
+		
+		// Error message should not appear
+		XCTAssertTrue(loginVC.errorMessageLabel.isHidden)
+		
+		// The alert should be presented
+		XCTAssertTrue(loginVC.presentedViewController is UIAlertController)
+	}
 }
